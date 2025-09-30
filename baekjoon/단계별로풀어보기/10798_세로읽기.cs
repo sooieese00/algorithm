cf354ff5b17de66108da4025846ac0d0e9264145
sooieese <sujinjang609@gmail.com>
2025-09-30 23:26:07 +0900

    Add: BOJ 10798 세로읽기

---
+++ baekjoon/단계별로풀어보기/10798_세로읽기.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = new string[5];  // 5줄 고정
+        int max = 0;
+
+        // 입력 받기
+        for (int i = 0; i < 5; i++)
+        {
+            string line = Console.ReadLine();
+            words[i] = line;
+
+            if (line.Length > max)
+            {
+                max = line.Length;
+            }
+        }
+
+        string result = "";
+
+        // 세로 읽기
+        for (int i = 0; i < max; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (words[j].Length<i+1)   // 문자열 길이 체크
+                {
+                    continue;
+                }
+                result += words[j][i];
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
