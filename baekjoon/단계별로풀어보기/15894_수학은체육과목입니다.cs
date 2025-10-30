28bdf66ec96607c91c2b26b44247e1ff42da5335
sooieese <sujinjang609@gmail.com>
2025-10-30 17:56:43 +0900

    Add: BOJ 15894 수학은 체육과목입니다

---
+++ baekjoon/단계별로풀어보기/15894_수학은체육과목입니다.cs
@@ -0,0 +1,11 @@
+// 4, 4-1+1+2+2 = 8, 8-2+1+2+3 = 12, 12-3+1+2+4 = 16
+
+using System;
+class BJ
+{
+    static void Main()
+    {
+        long a = long.Parse(Console.ReadLine());
+        Console.WriteLine(4*a);
+    }
+}
