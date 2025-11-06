001cd1dc2691182dc09a05d88b5c1c991171b732
sooieese <sujinjang609@gmail.com>
2025-11-06 18:47:46 +0900

    Add: BOJ 알고리즘 수업

--- baekjoon/단계별로풀어보기/24267_알고리즘수업-알고리즘의수행시간6.cs
+++ baekjoon/단계별로풀어보기/24267_알고리즘수업-알고리즘의수행시간6.cs
@@ -5,7 +5,7 @@ class BJ
     static void Main()
     {
         long n = long.Parse(Console.ReadLine());
-        Console.WriteLine(n*n*n);
+        Console.WriteLine(n * (n - 1) * (n - 2) / 6);
         Console.WriteLine(3);
     }
 }
